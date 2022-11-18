5fcb3c413ab7c80e9beb279a5d59ba841f8c4c42
RMAV <robertomartinsv11@gmail.com>
2022-11-18 14:09:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
-from . import crud, models, schemas
-from .database import SessionLocal, engine
+import crud, models, schemas
+from database import SessionLocal, engine
 
 models.Base.metadata.create_all(bind=engine)
 
