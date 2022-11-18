634da84b2acd770a977c2ac0db450333d3277917
RMAV <robertomartinsv11@gmail.com>
2022-11-18 14:10:00 -0300

    Update models.py

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, DateTime, Float, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from .database import Base
+from database import Base
 
 class Data(Base):
     __tablename__ = "data"
