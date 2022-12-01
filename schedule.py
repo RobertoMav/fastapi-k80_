12b859c26d43152ec6cdb4d19809759c5ff5669e
RMAV <robertomartinsv11@gmail.com>
2022-12-01 14:59:16 -0300

    Adding DAG to run on airflow

--- schedule.py
+++
@@ -1,9 +0,0 @@
-from functions import to_json
-import requests
-
-def post():
-    data = to_json()
-    request = requests.post("https://fastapi-production-549d.up.railway.app/data/", data=data)
-    return request, request.json()
-
-post()
